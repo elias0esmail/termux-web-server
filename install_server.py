f0be10ae409d27fe2c94d2225a9e558ed33a664f
elias0esmail <eliasusa00@gmail.com>
2026-09-21 20:35:55 +0300

    add reinstall option

--- install_server.py
+++ install_server.py
@@ -374,7 +374,7 @@ show_banner_and_status() {{
 
     echo -e "\\033[1;33m============= [ DEVELOPER INFO ] ==============\\033[0m"
     echo -e " Developer : \\033[1;32mElias Esmail\\033[0m"
-    echo -e " WhatsApp  : \\033[1;32mhttps://api.whatsapp.com/send?phone=967771902342\\033[0m"
+    echo -e " WhatsApp  : \\033[1;32m+967771902342\\033[0m"
     echo -e " GitHub    : \\033[1;36mhttps://github.com/elias0esmail\\033[0m"
     echo -e "\\033[1;33m================================================\\033[0m\\n"
 
