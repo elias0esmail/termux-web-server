c791447d7d2ebf2d1c4b536b92b95f087927f8be
elias0esmail <eliasusa00@gmail.com>
2026-09-25 18:32:58 +0300

    fix new feuture

--- install_server.py
+++ install_server.py
@@ -10,7 +10,7 @@ import secrets
 import getpass
 from pathlib import Path
 
-CURRENT_VERSION = "2.19.3"
+CURRENT_VERSION = "2.19.4"
 CHANGELOG = [
     "Fix: MariaDB root auth — use normal auth (no more ERROR 1698)",
     "New: Auto-repair broken unix_socket root auth on existing installs",
@@ -1192,7 +1192,7 @@ show_banner_and_status() {{
     echo " | |  | | |_| |____) |  __/| |    \ V /  __/ |       "
     echo " |_|  |_|\__, |_____/ \___||_|     \_/ \___|_|       "
     echo "          __/ |                                     "
-    echo "         |___/        Server Manager v{CURRENT_VERSION}  "
+    echo "         |___/        Server Manager vvv{CURRENT_VERSION}  "
     echo -e "\033[0m"
     echo -e "\033[1;33m============= [ DEVELOPER INFO ] ==============\033[0m"
     echo -e " Developer : \033[1;32mElias Esmail\033[0m"
